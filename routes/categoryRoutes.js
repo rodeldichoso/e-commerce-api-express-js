6d1eeeff4b2f0ee7f4564d23ea5e77094f315a13
rodeldichoso <rodeldichoso1133@gmail.com>
2025-07-13 12:28:23 +0800

    Complete CRUD for categories

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 const { validateCategoryInputs } = require('../middleware/inputValidator/categoryValidator');
 const validated = require('../middleware/validate');
-const { addCategory } = require('../controllers/categoryController');
+const { addCategory, getCategories, getOneCategory, updateCategory, deleteCategory } = require('../controllers/categoryController');
 const { authenticateToken } = require('../middleware/authUser');
 
 router.use(authenticateToken);
 
 router.post('/', validateCategoryInputs, validated, addCategory);
+router.get('/', getCategories);
+router.get('/:category_id', getOneCategory);
+router.put('/:category_id', updateCategory);
+router.delete('/:category_id', deleteCategory);
 
 module.exports = router;
